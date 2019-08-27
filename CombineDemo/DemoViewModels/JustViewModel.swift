a56e8c9283f7d23236bb8393d8d988734b74a03c
kevinjohnason <kevinjohnason@gmail.com>
2019-08-26 21:01:36 -0700

    added individual view model

---
+++ CombineDemo/DemoViewModels/JustViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  JustViewModel.swift
+//  CombineDemo
+//
+//  Created by Kevin Minority on 8/19/19.
+//  Copyright © 2019 Kevin Cheng. All rights reserved.
+//
+
+import Foundation
+import Combine
+class JustViewModel: StreamViewModel<String> {
+    
+    required init() {
+        super.init(title: "Just(\"A\")", publisher: Just("A").eraseToAnyPublisher())
+    }
+    
+}
